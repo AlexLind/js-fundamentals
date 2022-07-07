7c6f1664cc5cc8c1da3a7f4fe4ce40f980245cae
Alexander Lind <alex.anonym@gmail.com>
2022-07-07 14:48:34 +0200

    Completed object-keys exercise

--- src/data-types/objects/object-keys.js
+++ src/data-types/objects/object-keys.js
@@ -16,11 +16,27 @@ const isbn13 = '978-0132350884'
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
 
+
 // Set this to the book name
-const name = ''
+const name = book.name
 
 // Set this to the isbn 10 value
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+// Redefine book category
+book.category = "Programming"
+
+// Add pages to book object
+book.pages = 464
+
+// add constant isbn13 to book object
+book.isbn.isbn13 = isbn13
+
+// Delete dimensions in book object
+delete book.dimensions
+
+// Delete asin in isbn object in book object
+delete book.isbn.asin
 
 // Do not edit this exported object
 module.exports = {
