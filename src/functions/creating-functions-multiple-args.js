bb1a08547c0418b891dc9e06b317897ec3660e9f
Alexander Lind <alex.anonym@gmail.com>
2022-07-12 16:16:55 +0200

    Did the multiple args exercise

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -10,6 +10,14 @@
 //
 // TODO: write code below
 
+function lowerToUpper(lower, upper) {
+  let outputArray = []
+  for (let i = lower; i <= upper; i++) {
+    outputArray.push(i)
+  }
+  return outputArray
+}
+
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -22,8 +30,24 @@
 //
 // TODO: write code below
 
+function srzBzns(string, number) {
+  let textFirst
+  if (typeof string === 'string') {
+    textFirst = string.toUpperCase() 
+  } else {
+    return console.log('Not a valid input, need to be a string.')
+  }
+  if (typeof number === 'number') {
+  return textFirst + '!'.repeat(number)
+  } else {
+    return console.log('Not a valid input, need to be a number.')
+  }
+}
+
+console.log(srzBzns('Alex', 5))
+
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: lowerToUpper, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: srzBzns // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
