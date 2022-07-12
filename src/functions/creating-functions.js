100d16eb7e280e705002f33e7cf9d2bec79b9857
Alexander Lind <alex.anonym@gmail.com>
2022-07-12 16:38:05 +0200

    Finished creating functions exercise

--- src/functions/creating-functions.js
+++ src/functions/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function addOne(num1) {
+  return num1 + 1
+}
 
 // Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -18,9 +21,16 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function happy(name) {
+  let nameCapitalized
+  if (typeof name === 'string') {
+    nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1)
+  }
+  return 'Hi, ' + nameCapitalized + ' :)'
+}
 
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function you defined to increment a number (the first TODO)
-  b: undefined // change undefined to be the name of the function you defined to say hi (the second TODO)
+  a: addOne, // change undefined to be the name of the function you defined to increment a number (the first TODO)
+  b: happy // change undefined to be the name of the function you defined to say hi (the second TODO)
 }
