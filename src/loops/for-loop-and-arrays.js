578ed8703fe7da2fccf704fea6ad3c5757b24e8f
Alexander Lind <alex.anonym@gmail.com>
2022-07-14 16:29:09 +0200

    rewrote for-loops-and-arrays using array methods and arrow functions

--- src/loops/for-loop-and-arrays.js
+++ src/loops/for-loop-and-arrays.js
@@ -6,35 +6,63 @@ let word = ''
 // TODO: Add code below this line to make the tests pass
 
 // Use a for loop to set the sum variable to the sum of all the values in nums
-sum = 0
-for (i = 0; i < nums.length; i++) {
-  sum += nums[i]
-}
+
+// sum = 0
+// for (i = 0; i < nums.length; i++) {
+//   sum += nums[i]
+// }
+// console.log(sum)
+
+nums.forEach(item => {
+  sum += item;
+})
 console.log(sum)
+
 // Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
-for (i = 0; i < nums.length; i++) {
-  doubledNums.push(nums[i] * 2)
-}
+
+// for (i = 0; i < nums.length; i++) {
+//   doubledNums.push(nums[i] * 2)
+// }
+
+nums.forEach(item => {
+  doubledNums.push(item * 2)
+})
 console.log(doubledNums)
+
 // Use a for loop to set word equal to all the letters in the letters array
-word = ''
-for (i = 0; i < letters.length; i++) {
-  word += letters[i]
-}
+
+// for (i = 0; i < letters.length; i++) {
+//   word += letters[i]
+// }
+letters.forEach(item => {
+  word += item
+})
 console.log(word)
 // Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
-for (i = 1; i < nums.length; i = i + 2) {
-  everySecondNum.push(nums[i])
-}
+
+// for (i = 1; i < nums.length; i = i + 2) {
+//   everySecondNum.push(nums[i])
+// }
+
+
+nums.filter((item, i) => {
+ if (i % 2 === 2 - 1) {
+  everySecondNum.push(item)
+ }
+})
 console.log(everySecondNum)
 
 // Use a for loop to populate numsReversed with the numbers from nums in reverse order
-const numsReversed = []
-for (i = nums.length - 1; i >= 0; i--) {
-  numsReversed.push(nums[i])
-}
+
+// const numsReversed = []
+// for (i = nums.length - 1; i >= 0; i--) {
+//   numsReversed.push(nums[i])
+// }
+
+const numsReversed = nums.reverse()
+
 console.log(numsReversed)
 // do not change below this line
 module.exports = {
